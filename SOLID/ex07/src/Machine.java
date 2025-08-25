f2ba1a6af7c1c57873ceb796998d120b83b032c9
BishalJena <bishal.space@gmail.com>
2025-08-25 10:53:41 +0530

    solved

--- SOLID/ex07/src/Machine.java
+++ SOLID/ex07/src/Machine.java
@@ -1,5 +1,11 @@
-interface Machine {
+public interface Printer {
     void print(String text);
+}
+
+public interface Scanner {
     void scan(String dstPath);
+}
+
+public interface Fax {
     void fax(String number);
 }
